1f67b8f77187b77093efe451abfe9024e57afd3e
IzumiShaka-desu <shakaaji29@gmail.com>
2022-02-24 20:49:05 +0700

    Initial Commit

---
+++ Tests/DatasourcesTests/DatasourcesTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Datasources
+
+final class DatasourcesTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Datasources().text, "Hello, World!")
+    }
+}
